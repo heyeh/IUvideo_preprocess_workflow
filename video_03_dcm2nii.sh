2ace58e6e91943ade20e30634531bf73786092fc
Ye <heyebrain@gmail.com>
2020-05-25 16:51:02 -0400

    Create video_03_dcm2nii.sh

---
+++ video_03_dcm2nii.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sub_id=$1
+ana_dir=$2
+sess=$3
+
+echo " Processing sub_${sub_id} session_${sess}"
+rm -r ${ana_dir}/Nifti/.heudiconv/${sub_id}/ses-${sess}
+docker run --rm -it -v ${ana_dir}:/base \
+                       nipy/heudiconv:latest \
+                       -d /base/tempDCM/{subject}/ses-{session}/*.dcm \
+                       -o /base/Nifti/ \
+                       -f /base/templates/heuristic_update.py \
+                       -s ${sub_id} -ss ${sess} -c dcm2niix -b --overwrite \
+ln -s -f ${ana_dir}/Nifti/.heudiconv/${sub_id}/ses-${sess}/info/dicominfo_ses-${sess}.tsv ${ana_dir}/derivatives/dicominfo_sub-${sub_id}_ses-${sess}.tsv
