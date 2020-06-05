ed1e19725b0750e97969e2257f7e90d1568fb68c
Ye <heyebrain@gmail.com>
2020-06-05 11:55:35 -0400

    Update preprocess_pipeline_v02.sh

--- preprocess_pipeline_v02.sh
+++ preprocess_pipeline_v02.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-proc_dir=XX/XX #where you put BIDs data and other necessary files
+proc_dir=XX/BIDS #where you put BIDs data and other necessary files
 scripts_dir=XX/scripts #where you put all your scripts
 subject_list=$( cat ${scripts_dir}/subjects.list ) #a list of subjects id
 rawdata_dir=XX/XX # your dicom directory
-mappingfile=XXX.csv # your log mapping file will be searched in $proc_dir/templates
+mappingfileName=XXX # input the content in { } as {initial_subs_runs}_sess01.csv --your log mapping file will be searched in $proc_dir/templates
 
 # mkdir -p ${proc_dir}/{Nifti,derivatives,templates,logfiles,tempDCM}
 
@@ -28,7 +28,7 @@ cd ${scripts_dir}
 # done
 ##################################################
 ## Step 2. Run heudiconv to generate tsv file of## 
-## scan info. for double check. After this step ##
+## scan info. for double check. After this step, ##
 ## remember to run doublecheck.m in MATLAB. Usually##
 ## the fieldmap series id might be different, which##
 ## need to be further check.                    ## 
@@ -42,7 +42,7 @@ cd ${scripts_dir}
 #      echo " ERROR:${subj} NO raw dicom;Exit now...... " 
 #      exit
 #   fi
-#   for sess in 01 02 03
+#   for sess in 01 02 #03
 #   do
 #      if [ ! -f ${proc_dir}/derivatives/dicominfo_sub-${subj}_ses-${sess}.tsv ]; then
 #         ./video_01_generate_seriesID.sh ${subj} ${rawdata_dir} ${proc_dir} ${sess}
@@ -59,16 +59,16 @@ cd ${scripts_dir}
 #   #  if [ -d ${proc_dir}/tempDCM/${subj} ]; then
 #   #    echo  -----------------------------------------------------
 #   #    echo  ${subj} exist in tempDCM; Exit now.....
-#   #    echo  You can decide if want to delete the existing dataset
+#   #    echo  " You can decide if want to delete the existing dataset "
 #   #    exit
 #   # fi 
 #  
-#    for sess in 01 02 03
+#    for sess in 01 02 #03
 #    do
 #      if [ ! -d ${proc_dir}/tempDCM/${subj}/ses-${sess} ]; then
-#          ./video_02_copydcm_update.sh ${subj} ${proc_dir} ${rawdata_dir} ${sess} ${mappingfile}>${proc_dir}/logfiles/copydcm_${subj}_sess-${sess}.log 2>&1
+#          ./video_02_copydcm_update.sh ${subj} ${proc_dir} ${rawdata_dir} ${sess} ${mappingfileName}_sess${sess}.csv > ${proc_dir}/logfiles/copydcm_${subj}_sess-${sess}.log 2>&1
 #      else
-#           echo " ${subj} ses-${sess} EXISTS in tempDCM " 
+#           echo " ${subj} ses-${sess} EXISTS in tempDCM " >${proc_dir}/logfiles/copydcm_${subj}_sess-${sess}.log 2>&1
 #      fi
 #    done
 # done
@@ -88,13 +88,13 @@ cd ${scripts_dir}
 #    #    echo " ERROR: NO ${subj} in tempDCM; Exit now...... "
 #    #    exit
 #    # fi
-#  for sess in 01 02 03
+#  for sess in 01 02 #03
 #  do
 #     if [ ! -d ${proc_dir}/Nifti/sub-${subj}/ses-${sess} ]; then
 #        ./video_03_dcm2nii.sh ${subj} ${proc_dir} ${sess} > ${proc_dir}/logfiles/dcm2nii_${subj}_sess-${sess}.log 2>&1
 #        ./video_04_assignFieldmap.sh ${subj} ${proc_dir}/Nifti ${sess} > ${proc_dir}/logfiles/assignfmp_${subj}_sess-${sess}.log 2>&1
 #     else
-#        echo " ${subj} ses-${sess} EXISTS in Nifti "
+#        echo " ${subj} ses-${sess} EXISTS in Nifti " > ${proc_dir}/logfiles/assignfmp_${subj}_sess-${sess}.log 2>&1
 #  done
 #done
 ### Run doublecheck.m again to see anything wrong. 
